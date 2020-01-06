fd8d42800fa37817325ed1497dba4f145f089079
Marcelo Silva <maarcelomonteiro@hotmail.com>
2020-01-06 10:45:01 -0300

    apply valitor to routes

--- start/routes.js
+++ start/routes.js
@@ -3,15 +3,19 @@
 const Route = use('Route')
 
 Route.post('users', 'UserController.store').validator('User')
-Route.post('session', 'SessionController.store')
-Route.post('forgout-password', 'ForgoutPasswordController.store')
-Route.put('forgout-password', 'ForgoutPasswordController.update')
+Route.post('session', 'SessionController.store').validator('Session')
+Route.post('forgout-password', 'ForgoutPasswordController.store').validator('ForgoutPassword')
+Route.put('forgout-password', 'ForgoutPasswordController.update').validator('ResetPassword')
 Route.get('files/:id', 'FileController.show')
 
 Route.group(() => {
 
   Route.post('files', 'FileController.store')
-  Route.resource('projects', 'ProjectController').apiOnly()
-  Route.resource('projects.tasks', 'TaskController').apiOnly()
+  Route.resource('projects', 'ProjectController')
+    .apiOnly()
+    .validator(new Map([[['project.store'], ['Project']]]))
+  Route.resource('projects.tasks', 'TaskController')
+  .apiOnly()
+  .validator(new Map([[['project.tasks.store'], ['Task']]]))
 
 }).middleware(['auth'])
