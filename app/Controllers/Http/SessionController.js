5466cfc987ebb95abaed4f95767881d7680e0a1c
Marcelo Silva <maarcelomonteiro@hotmail.com>
2020-01-06 11:11:51 -0300

    add handle response to error

--- app/Controllers/Http/SessionController.js
+++ app/Controllers/Http/SessionController.js
@@ -2,10 +2,13 @@
 
 class SessionController {
   async store({ request, response, auth }) {
-    const { email, password } = request.all()
-    const token = await auth.attempt(email, password)
-    return token
+    try {
+      const { email, password } = request.all()
+      const token = await auth.attempt(email, password)
+      return token
+    } catch (error) {
+      return response.status(error.status).send({ error: 'Ocorreu um erro' })
+    }
   }
 }
-
 module.exports = SessionController
